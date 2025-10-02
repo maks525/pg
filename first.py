ae5a8d4b3276181bd1892f28be060d63b6923e0f
maks525 <maksat.serik06@gmail.com>
2025-10-02 12:39:07 +0000

    domaci ukol 1

---
+++ first.py
@@ -0,0 +1,15 @@
+def sudy_nebo_lichy(cislo):
+    
+
+
+
+if __name__ == 'main':
+    
+    cislo = input('Zadejte cislo ')
+    cislo = int(cislo)
+
+    print('Cislo je: ', cislo)
+    
+    
+    sudy_nebo_lichy(5)
+    sudy_nebo_lichy(1000000)
