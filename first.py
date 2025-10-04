9f08213ee5648195a75612304cc5ac7d290da777
maks525 <maksat.serik06@gmail.com>
2025-10-04 23:15:13 +0000

    domaci ukol

--- first.py
+++ first.py
@@ -1,15 +1,18 @@
-def sudy_nebo_lichy(cislo):
-    
+def sudy_nebo_lichy(hodnota):
+    if (hodnota % 2) == 0:
+        print(f'cislo {hodnota} je sudy')
+    else:
+        print(f'cislo {hodnota} je lichy')
 
 
 
-if __name__ == 'main':
+if __name__ == '__main__':
     
     cislo = input('Zadejte cislo ')
     cislo = int(cislo)
 
-    print('Cislo je: ', cislo)
-    
+    print(f'Cislo je: {cislo}')
     
     sudy_nebo_lichy(5)
     sudy_nebo_lichy(1000000)
+    sudy_nebo_lichy(cislo)
